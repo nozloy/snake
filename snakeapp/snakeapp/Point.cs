df204c5a197efa2209799b908c1174f0c0ad0a3e
nozloy <nozloy@gmail.com>
2018-11-11 05:24:57 +0300

    Классы и объекты

---
+++ snakeapp/snakeapp/Point.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snakeapp
+{
+    class Point
+    {
+        public int x;
+        public int y;
+        public char sym;
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+    }
+}
